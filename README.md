749537145b792d8dd1a16603e254654d57c90135
Ryan Grove <ryan@wonko.com>
2018-05-15 11:40:15 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Benchmarks for rgrove/parse-xml
+
+This repo contains benchmarks for [rgrove/parse-xml](https://github.com/rgrove/parse-xml).
+See the main repo for more details.
+
+## Running Benchmarks
+
+Clone this repo, then run:
+
+```
+npm i && npm run benchmark
+```
+
+## License
+
+[ISC License](LICENSE)
---
+++ index.js
@@ -0,0 +1,45 @@
+/* globals bench, set, suite */
+'use strict';
+
+const fs = require('fs');
+const libxmljs = require('libxmljs');
+const os = require('os');
+const parseXml = require('@rgrove/parse-xml');
+const xmldoc = require('xmldoc');
+
+console.log(`Node.js ${process.version} / ${os.type()} ${os.arch()}\n${os.cpus()[0].model}`);
+
+[
+  {
+    name: 'Small document',
+    filename: `${__dirname}/fixtures/small.xml`
+  },
+
+  {
+    name: 'Medium document',
+    filename: `${__dirname}/fixtures/medium.xml`
+  },
+
+  {
+    name: 'Large document',
+    filename: `${__dirname}/fixtures/large.xml`
+  }
+].forEach(({ filename, name }) => {
+  let xml = fs.readFileSync(filename, { encoding: 'utf8' });
+
+  suite(`${name} (${xml.length} bytes)`, () => {
+    set('delay', 100);
+
+    bench('libxmljs (native)', () => {
+      libxmljs.parseXml(xml);
+    });
+
+    bench('parse-xml', () => {
+      parseXml(xml);
+    });
+
+    bench('xmldoc (sax-js)', () => {
+      new xmldoc.XmlDocument(xml);
+    });
+  });
+});
